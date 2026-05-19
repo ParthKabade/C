8a48dc720a56a690d7b145cc71f96d9668d88211
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-20 01:08:37 +0530

    Add files via upload

---
+++ FunctionDemoXX.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+void Display()
+{
+
+    printf("Jay Ganesh....\n");
+}
+
+
+int main()
+
+{
+    Display();
+    Display();
+    Display();
+
+
+    return 0;
+}
---
+++ ParametersX.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+
+void Display(int No1,int No2)
+{
+
+    printf("%d %d\n",No1,No2);
+}
+s
+
+int main()
+
+{
+    Display(11,22);
+    
+
+
+    return 0;
+}

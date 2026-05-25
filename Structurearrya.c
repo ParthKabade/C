3873a2d11b9f3d1a861e40d1d423b51459e51314
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-26 00:25:09 +0530

    Add files via upload

---
+++ Structurearrya.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+//Declaration
+struct Hello
+{
+    float f;  //4
+    int Arr[3]; //12
+               
+}hobj;          //16 
+
+
+int main()
+{
+    printf("Size of object is: %lu\n",sizeof(hobj));
+
+
+    
+    return 0;
+}

0efb3a73806cf40c8521a8b302a865543de4e95a
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-28 00:41:07 +0530

    Add files via upload

---
+++ FunctionDemoX.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+void parimal()
+{
+
+    printf("arranging chairs....\n");
+}
+
+
+int main()
+
+{
+    parimal();
+    parimal();
+    parimal();
+    parimal();
+    parimal();
+
+
+
+    return 0;
+}

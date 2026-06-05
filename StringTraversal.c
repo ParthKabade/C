e3fa6c0f19a10ada1241d22780b7babc8f4e594c
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-06-06 00:17:04 +0530

    Add files via upload

---
+++ StringTraversal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char Str[] = "Ganesh";
+
+    char *ptr = NULL;
+
+    ptr = Str; // 100
+
+    while ((*ptr != '\0'))
+    {
+        printf("%c\n", *ptr); // G
+        ptr++;                // 101 
+    }
+    
+
+    return 0;
+}

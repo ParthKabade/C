e698c245a8ec526bbf110e6756c8fcf2052df167
Parth Kabade <151639939+ParthKabade@users.noreply.github.com>
2026-08-27 01:16:13 +0530

    Add files via upload

---
+++ program97.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int iArr[7];
+
+int main()
+{
+    
+    iArr[0]=10;
+    iArr[3]=20;
+    iArr[6]=30;
+
+    printf("%d\n",sizeof(iArr));
+    printf("%d\n",iArr[0]);
+    printf("%d\n",iArr[3]);
+    printf("%d\n",iArr[6]);
+    
+    printf("%d\n",iArr[2]);
+    printf("%d\n",iArr[5]);
+   
+    
+    return 0;
+}

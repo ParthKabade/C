7d598fbf1db8d620454c7fb6475e239ffa038931
Parth Kabade <151639939+ParthKabade@users.noreply.github.com>
2026-09-01 01:15:27 +0530

    Add files via upload

---
+++ program119.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+//Error due to stdlib.h header file missing
+
+int main()
+{
+    int *Brr=NULL;
+    int iLength=0,iCnt=0;
+
+    //step 1:Accept the number of elements
+    printf("Enter number of elements :\n");
+    scanf("%d",&iLength);
+
+    //step 2: Allocate the memory
+    Brr=(int *)malloc(iLength * sizeof(int));
+
+    //step 3:Accept the valuees from user
+    for(iCnt=0;iCnt<iLength;iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    //step 4: Use the memory(Logic)
+
+
+    //step 5: Deallocate the memory
+    free(Brr);
+
+    return 0;
+}

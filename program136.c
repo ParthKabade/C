b2b58254ea2d71418bd375de4f875402cd938372
Parth Kabade <151639939+ParthKabade@users.noreply.github.com>
2026-09-04 02:23:49 +0530

    Add files via upload

---
+++ program136.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//O(N) Time complexity
+int Minimum(int Arr[],int iSize)
+{
+    int iMin=0;
+    int iCnt=0;
+
+    iMin=Arr[0];
+
+    for(iCnt=0;iCnt<iSize;iCnt++)
+    {
+        if(Arr[iCnt]<iMin)
+        {
+            iMin=Arr[iCnt];
+        }
+    }
+
+    return iMin;
+    
+}
+
+int main()
+{
+
+    int *Brr=NULL;
+    int iLength=0,iCnt=0,iRet=0;
+
+    printf("Enter the number of elements : \n");
+    scanf("%d",&iLength);
+
+    Brr=(int *)malloc(sizeof(int)*iLength);
+
+    printf("Enter the elements :\n");
+
+    for(iCnt=0;iCnt<iLength;iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet=Minimum(Brr,iLength);
+
+    printf("Minimum number is : %d\n",iRet);
+
+    free(Brr);
+
+    return 0;
+}
